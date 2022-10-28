e6d139faca42321a61a0c4409e7a1a783a14831c
Mardoqueu <mdq767@gmail.com>
2022-10-28 09:22:10 -0300

    handling secrect key of emailjs

--- .history/src/components/contact/Contact_20221028091720.jsx
+++ .history/src/components/contact/Contact_20221028091720.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, 'nWF4zILsQtefvfQm7')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
